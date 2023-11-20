7f247f706feb3bdb24720dc0d01a928814f89e5b
Julio Domínguez <jdominguqy@alumnos.unex.es>
2023-11-20 18:41:33 +0100

    Squashed commit of the following:
    commit 228266b6014859a86ffd532e1ddc19ba41c4a592
    Author: fortizva <fortizva@alumnos.unex.es>
    Date:   Sun Nov 19 21:09:37 2023 +0100
    
        GD05-54 - Navegación de “Mi Inventario” a “Pieza Nº”
    
        + Añadido enlace desde vista de "Mi inventario" a detalles de una pieza
    
    commit 4bdb99e5608a86ae4b72e0ad0398ff148d890921
    Author: fortizva <fortizva@alumnos.unex.es>
    Date:   Sun Nov 19 21:00:38 2023 +0100
    
        GD05-53 - Crear pantalla “Pieza Nº”
    
        + Añadida pantalla de detalles de pieza
    
    commit ca1321fc999651566725277876592048c311ea24
    Merge: 7fefc3a bd1489a
    Author: fortizva <fortizva@alumnos.unex.es>
    Date:   Sun Nov 19 17:18:36 2023 +0100
    
        Merge branch 'GD05-14-Consultar-Inventario' of github-uex.com:UniExtremadura/gps-project-gd-05 into GD05-14-Consultar-Inventario
    
    commit 7fefc3a495c95553661f412baf8e23f0da5fce6a
    Author: fortizva <fortizva@alumnos.unex.es>
    Date:   Sun Nov 19 17:16:48 2023 +0100
    
        GD05-52 - Crear pantalla “Mi Inventario”
    
        + Añadido fragmento de "Mi inventario"
        + Añadido RecyclerView con Adapter para "Mi Inventario"
        + Añadida DummyData
    
    commit bd1489abfdb3880b20527731e7c079e08f517fee
    Author: Javier <jgarciatu@alumnos.unex.es>
    Date:   Thu Nov 16 22:18:41 2023 +0100
    
        GD05-57 Crear método para consultar todas las piezas
    
        BrickDao operaciones dao implementadas
        BrickSetDao operaciones dao implementadas
        CategoryDao operaciones dao implementadas
        ThemeDao operaciones dao implementadas
        HomeActivity se ha añadido un método pruebaInser, con este podemos comprobar mediante el AppInspector que se ha creado correctamente la BD
    
    commit d4055fb83a34b75551e4eca15858a163ed2e0156
    Author: Javier <jgarciatu@alumnos.unex.es>
    Date:   Thu Nov 16 22:12:26 2023 +0100
    
        GD05-56 Crear clase de consulta a BD
    
        +BrickDao dao para realizar las operaciones a la BD
        +BrickSetDao dao para realizar las operaciones a la BD
        +CategoryDao dao para realizar las operaciones a la BD
        +ThemeDao dao para realizar las operaciones a la BD
    
    commit 8a861e861592e392d02ed526a0e226e6326ccf0a
    Author: Javier <jgarciatu@alumnos.unex.es>
    Date:   Thu Nov 16 21:25:34 2023 +0100
    
        GD05-55 Crear base de datos Room
    
        Se ha creado la clase de la base de datos Room
        +Category clase para almacenar las categorías
        +Theme clase para almacenar las temáticas
        +BrickSetWithBricks clase para almacenar el conjunto de Bricks que forman un BrickSet
        +BrickSetBrickCrossRef clase para almacenar en la base de datos la relación entre BrickSet y Brick
        En BrickSet se han añadido las anotaciones Room, se han modificado los nombres de algunas variables y se ha eliminado el atributo Bricks
        En Brick se han añadido las anotaciones Room y modificado los nombres de algunas variables
    
    commit 31accd14eb7311cffb569d320c50244a05b5067e
    Author: fortizva <fortizva@alumnos.unex.es>
    Date:   Wed Nov 15 19:59:35 2023 +0100
    
        GD05-51 - Crear clase de datos local
    
        + Implementada clase Brick
        + Implementada clase BrickSet

---
+++ app/src/main/java/com/gd05/brickr/dummy/DummyData.kt
@@ -0,0 +1,76 @@
+package com.gd05.brickr.dummy
+
+import com.gd05.brickr.model.Brick
+
+val dummyBricks: List<Brick> = listOf(
+    Brick(
+        26047,
+        "Plate Special 1 x 1 Rounded with Handle",
+        9,
+        2016,
+        2023,
+        "https://rebrickable.com/parts/26047/",
+        "https://cdn.rebrickable.com/media/parts/elements/6157554.jpg",
+        1
+    ),
+    Brick(
+        26047,
+        "Plate Special 1 x 1 Rounded with Handle",
+        9,
+        2016,
+        2023,
+        "https://rebrickable.com/parts/26047/",
+        "https://cdn.rebrickable.com/media/parts/elements/6157554.jpg",
+        1
+    ),
+    Brick(
+        26047,
+        "Plate Special 1 x 1 Rounded with Handle",
+        9,
+        2016,
+        2023,
+        "https://rebrickable.com/parts/26047/",
+        "https://cdn.rebrickable.com/media/parts/elements/6157554.jpg",
+        1
+    ),
+    Brick(
+        26601,
+        "Wedge Plate 2 x 2 Cut Corner",
+        49,
+        2016,
+        2023,
+        "https://rebrickable.com/parts/26047/",
+        "https://cdn.rebrickable.com/media/parts/elements/6176240.jpg",
+        1
+    ),
+    Brick(
+        26601,
+        "Wedge Plate 2 x 2 Cut Corner",
+        49,
+        2016,
+        2023,
+        "https://rebrickable.com/parts/26047/",
+        "https://cdn.rebrickable.com/media/parts/elements/6176240.jpg",
+        1
+    ),
+    Brick(
+        26601,
+        "Wedge Plate 2 x 2 Cut Corner",
+        49,
+        2016,
+        2023,
+        "https://rebrickable.com/parts/26047/",
+        "https://cdn.rebrickable.com/media/parts/elements/6176240.jpg",
+        1
+    ),
+    Brick(
+        26601,
+        "Wedge Plate 2 x 2 Cut Corner",
+        49,
+        2016,
+        2023,
+        "https://rebrickable.com/parts/26047/",
+        "https://cdn.rebrickable.com/media/parts/elements/6176240.jpg",
+        1
+    )
+)
---
+++ app/src/main/java/com/gd05/brickr/model/Brick.kt
@@ -0,0 +1,41 @@
+package com.gd05.brickr.model
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+/** Class that represents a brick of the Lego API **/
+
+@Entity(foreignKeys = [ForeignKey(entity = Category::class,
+    parentColumns = arrayOf("categoryId"),
+    childColumns = arrayOf("categoryId"),
+    onDelete = ForeignKey.CASCADE)]
+)
+data class Brick(
+    /** Brick's identification number **/
+    @PrimaryKey
+    val brickId: Int,
+
+    /** Brick's readable name **/
+    val name: String,
+
+    /** Brick's category identification number **/
+    val categoryId: Int,
+
+    /** Year when the Brick was released **/
+    val yearFrom: Int,
+
+    /** Year when the Brick was discontinued **/
+    val yearTo: Int,
+
+    /** Brick's Rebrickable URL **/
+    val brickUrl: String,
+
+    /** Brick's image URL **/
+    val brickImgUrl: String,
+
+    /** Brick amount in inventory **/
+    val amount: Int
+
+) : Serializable
