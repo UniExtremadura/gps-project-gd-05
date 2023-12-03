0f51ff733d09484a4b0899a1dac70fd2ad92f161
Julio Domínguez <jdominguqy@alumnos.unex.es>
2023-12-03 13:10:42 +0100

    GD05-101 - Crear botón "Eliminar piezas" de mi inventario
    Añadido mensaje en el adapter para mostrar correctamente las cantidades

--- app/src/main/java/com/gd05/brickr/ui/home/BrickSetPartsAdapter.kt
+++ app/src/main/java/com/gd05/brickr/ui/home/BrickSetPartsAdapter.kt
@@ -6,19 +6,22 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.gd05.brickr.R
+import com.gd05.brickr.database.dao.BrickSetDao
 import com.gd05.brickr.databinding.BricksetPartsItemListBinding
 import com.gd05.brickr.model.Brick
 
-class BrickSetPartsAdapter (
+class BrickSetPartsAdapter(
 
-private var bricks: List<Brick>,
-private var amounts: Map<String, Int>,
-private val onClick: (brick: Brick) -> Unit,
-private val onLongClick: (title: Brick) -> Unit,
-private val onRemoveClick: (brick: Brick) -> Unit,
-private val onAddClick: (brick: Brick) -> Unit,
-private val context: Context?
+    private var bricks: List<Brick>,
+    private var amounts: Map<String, Int>,
+    private val onClick: (brick: Brick) -> Unit,
+    private val onLongClick: (title: Brick) -> Unit,
+    private val onRemoveClick: (brick: Brick) -> Unit,
+    private val onAddClick: (brick: Brick) -> Unit,
+    private val context: Context?,
+    private var localAmount: Map<String, Int>
 ) : RecyclerView.Adapter<BrickSetPartsAdapter.BrickSetPartViewHolder>() {
+
     class BrickSetPartViewHolder(
         private val binding: BricksetPartsItemListBinding,
         private val onClick: (brick: Brick) -> Unit,
@@ -27,12 +30,13 @@ private val context: Context?
         private val onRemoveClick: (brick: Brick) -> Unit,
         private val context: Context?,
     ) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(brick: Brick, amounts: Map<String, Int>, totalItems: Int) {
+        fun bind(brick: Brick, amounts: Map<String, Int>, localAmounts: Map<String, Int>) {
             with(binding) {
                 bricksetPartTitle.text = brick.name
-                bricksetPartId.text = "#${brick.brickId.toString()}"
+                bricksetPartId.text = "#${brick.brickId}"
                 /* TODO: Add needed bricks and owned bricks */
-                bricksetPartAmount.text = "-1 / ${amounts[brick.brickId] ?: -1}";
+                bricksetPartAmount.text =
+                    "${localAmounts[brick.brickId]} / ${amounts[brick.brickId]}"
 
                 /* Add images to bricks */
                 context?.let {
@@ -62,18 +66,32 @@ private val context: Context?
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BrickSetPartViewHolder {
-        val binding = BricksetPartsItemListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return BrickSetPartViewHolder(binding, onClick, onLongClick, onAddClick, onRemoveClick, context)
+        val binding =
+            BricksetPartsItemListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return BrickSetPartViewHolder(
+            binding,
+            onClick,
+            onLongClick,
+            onAddClick,
+            onRemoveClick,
+            context
+        )
     }
 
     override fun getItemCount() = bricks.size
 
     override fun onBindViewHolder(holder: BrickSetPartViewHolder, position: Int) {
-        holder.bind(bricks[position], amounts ,bricks.size)
+        holder.bind(bricks[position], amounts, localAmount)
     }
 
-    fun updateData(newBricks: List<Brick>) {
+    fun updateData(
+        newBricks: List<Brick>,
+        newAmount: Map<String, Int>,
+        localAmount: Map<String, Int>
+    ) {
+        this.amounts = newAmount
         this.bricks = newBricks
+        this.localAmount = localAmount
         notifyDataSetChanged()
     }
 }
