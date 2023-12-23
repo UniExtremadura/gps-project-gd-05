6c41fc4cfcfccab64cc9539e9700517b9f844638
Javier <jgarciatu@alumnos.unex.es>
2023-12-23 09:59:39 +0100

    GD05-112 Test Guardar pieza en mi inventario
    Añadido Test Espresso para comprobar el guardado de piezas en mi Inventario

---
+++ app/src/androidTest/java/com/gd05/brickr/ui/home/GuardarPiezaTest.kt
@@ -0,0 +1,156 @@
+package com.gd05.brickr.ui.home
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.gd05.brickr.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class GuardarPiezaTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(HomeActivity::class.java)
+
+    @Test
+    fun guardarPiezaTest() {
+        val appCompatToggleButton = onView(
+            allOf(
+                withId(R.id.search_bricks_button), withText("Piezas"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.LinearLayout")),
+                        0
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatToggleButton.perform(click())
+
+        val actionMenuItemView = onView(
+            allOf(
+                withId(R.id.search_view), withContentDescription("B�squeda\n"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.toolbar),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        actionMenuItemView.perform(click())
+
+        val searchAutoComplete = onView(
+            allOf(
+                withId(com.google.android.material.R.id.search_src_text),
+                childAtPosition(
+                    allOf(
+                        withId(com.google.android.material.R.id.search_plate),
+                        childAtPosition(
+                            withId(com.google.android.material.R.id.search_edit_frame),
+                            1
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        searchAutoComplete.perform(replaceText("batman"), closeSoftKeyboard())
+        Thread.sleep(2000)
+        val cardView = onData(
+            allOf(
+                withId(R.id.cv_Item),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.cl_item),
+                        childAtPosition(
+                            withId(R.id.rv_search_list),
+                            0
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        cardView.perform(click())
+        Thread.sleep(2000)
+        val materialButton = onView(
+            allOf(
+                withId(R.id.brick_details_add), withText("+"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.sv_brick_details),
+                        0
+                    ),
+                    7
+                )
+            )
+        )
+        materialButton.perform(scrollTo(), click())
+
+        val bottomNavigationItemView = onView(
+            allOf(
+                withId(R.id.InventoryFragment), withContentDescription("Inventario"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.bottom_navigation),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView.perform(click())
+
+        val textView = onView(
+            allOf(
+                withId(R.id.inventory_amount), withText("1"),
+                withParent(withParent(withId(R.id.cv_Item))),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("1")))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
